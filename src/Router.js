049ea67693b22c08c8f46affff9d0d497b15d799
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-01-28 23:09:26 -0200

    add employees list screen

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Scene, Roter, Router } from 'react-native-router-flux';
+import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 export default () => {
   return (
     <Router>
       <Scene key='root'>
-        <Scene key='login' component={LoginForm} title="Sign in" />
+        <Scene key='login' component={LoginForm} title="Sign in" initial />
+        <Scene key='employeeList' component={EmployeeList} title="Employees" />
       </Scene>
     </Router>
   );
---
+++ src/components/EmployeeList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text> employee </Text>
+        <Text> employee </Text>
+        <Text> employee </Text>
+        <Text> employee </Text>
+        <Text> employee </Text>
+        <Text> employee </Text>
+      </View>
+    );
+  }
+}
